107d89568dec661ba4036c31322ed1cc6a268bb9
Raphael Barth <raphaelbvestenacapri@gmail.com>
2026-03-27 15:33:37 -0300

    h2 added

---
+++ src/main/java/pratica/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package pratica.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String boasVindas() {
+        return  "se liga no pai kkk";
+    }
+}
